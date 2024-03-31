1a2a06cd532ec37334435a17dec109ccac734cfc
paz <paz@twowaysix.com>
2024-03-31 18:40:09 +0100

    reading resolution from midi file now

--- internal/midi/importing.go
+++ internal/midi/importing.go
@@ -13,19 +13,35 @@ import (
 )
 
 func ImportFromReader(reader io.Reader) (clonehero.Chart, error) {
+	smfData, err := smf.ReadFrom(reader)
+	if err != nil {
+		return clonehero.Chart{}, err
+	}
+
+	resolution := 960
+	switch smfData.TimeFormat.(type) {
+	case smf.MetricTicks:
+		resolution = int(smfData.TimeFormat.(smf.MetricTicks))
+	}
+
+	var buffer bytes.Buffer
+	_, err = smfData.WriteTo(&buffer)
+	if err != nil {
+		return clonehero.Chart{}, err
+	}
+
 	noteEvents := make([]clonehero.NoteEvent, 0)
 	tempos := make([]clonehero.Tempo, 0)
 	timeSignatures := make([]clonehero.TimeSignature, 0)
 
-	smf.ReadTracksFrom(reader).Do(func(ev smf.TrackEvent) {
-		fmt.Printf("track %v %d @%vms %s\n", ev.TrackNo, ev.AbsTicks, ev.AbsMicroSeconds/1000, ev.Message)
+	smf.ReadTracksFrom(&buffer).Do(func(ev smf.TrackEvent) {
+		// fmt.Printf("track %v %d @%vms %s\n", ev.TrackNo, ev.AbsTicks, ev.AbsMicroSeconds/1000, ev.Message)
 
 		var channel uint8
 		var key uint8
 		var velocity uint8
 		isNoteOn := ev.Message.GetNoteOn(&channel, &key, &velocity)
 		if isNoteOn {
-			fmt.Println("isNoteOn")
 			if velocity == 0 {
 				return
 			}
@@ -79,7 +95,7 @@ func ImportFromReader(reader io.Reader) (clonehero.Chart, error) {
 		},
 		Song: clonehero.SongSection{
 			Offset:       0,
-			Resolution:   192,
+			Resolution:   resolution,
 			Player2:      "Player2",
 			Difficulty:   3,
 			PreviewStart: 0,
@@ -108,81 +124,4 @@ func ImportFile(filePath string) (clonehero.Chart, error) {
 	reader := bytes.NewReader(data)
 
 	return ImportFromReader(reader)
-
-	// noteEvents := make([]clonehero.NoteEvent, 0)
-	// tempos := make([]clonehero.Tempo, 0)
-	// timeSignatures := make([]clonehero.TimeSignature, 0)
-
-	// smf.ReadTracksFrom(reader).Do(func(ev smf.TrackEvent) {
-	// 	fmt.Printf("track %v %d @%vms %s\n", ev.TrackNo, ev.AbsTicks, ev.AbsMicroSeconds/1000, ev.Message)
-
-	// 	var channel uint8
-	// 	var key uint8
-	// 	var velocity uint8
-	// 	isNoteOn := ev.Message.GetNoteOn(&channel, &key, &velocity)
-	// 	if isNoteOn {
-	// 		part, ok := DrumMapping[MidiNote(key)]
-	// 		if !ok {
-	// 			return
-	// 		}
-
-	// 		notes := clonehero.StylesForPart(part)
-	// 		for _, note := range notes {
-	// 			noteEvents = append(noteEvents, clonehero.NoteEvent{
-	// 				Position: int(ev.AbsTicks),
-	// 				Note:     note,
-	// 			})
-	// 		}
-	// 		return
-	// 	}
-
-	// 	var bpm float64
-	// 	isTempo := ev.Message.GetMetaTempo(&bpm)
-	// 	if isTempo {
-	// 		tempos = append(tempos, clonehero.Tempo{
-	// 			Position: int(ev.AbsTicks),
-	// 			MilliBPM: int(bpm * 1000),
-	// 		})
-	// 		return
-	// 	}
-
-	// 	var numerator uint8
-	// 	var denominator uint8
-	// 	var clocksPerClick uint8
-	// 	var demiSemiQuaverPerQuarter uint8
-	// 	isTimeSig := ev.Message.GetMetaTimeSig(&numerator, &denominator, &clocksPerClick, &demiSemiQuaverPerQuarter)
-	// 	if isTimeSig {
-	// 		denominatorExp := math.Sqrt(float64(denominator))
-	// 		timeSignatures = append(timeSignatures, clonehero.NewTimeSignature(
-	// 			int(ev.AbsTicks),
-	// 			int(numerator),
-	// 			int(denominatorExp),
-	// 		))
-	// 		return
-	// 	}
-
-	// })
-
-	// chart := clonehero.Chart{
-	// 	ExpertDrums: clonehero.ExpertDrumSection{
-	// 		Values: noteEvents,
-	// 	},
-	// 	Song: clonehero.SongSection{
-	// 		Offset:       0,
-	// 		Resolution:   192,
-	// 		Player2:      "Player2",
-	// 		Difficulty:   0,
-	// 		PreviewStart: 0,
-	// 		PreviewEnd:   0,
-	// 		Genre:        "Genre",
-	// 		MediaType:    "MediaType",
-	// 	},
-	// 	Events: clonehero.EventSection{},
-	// 	SyncTrack: clonehero.SyncTrackSection{
-	// 		TimeSignatures: timeSignatures,
-	// 		Tempos:         tempos,
-	// 	},
-	// }
-
-	// return chart, nil
 }
